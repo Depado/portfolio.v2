3890bb063b2c6ddddcd35359fdb05aa447906e5d
Depado <paul.lhussiez@gmail.com>
2016-04-07 15:46:41 +0200

    Removing useless rice dependency, and grasping Go template concepts

--- main.go
+++ main.go
@@ -1,43 +1,35 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
-	"github.com/GeertJohan/go.rice"
 	"github.com/gin-gonic/gin"
 
 	"github.com/Depado/portfolio.v2/fetch"
-	"github.com/Depado/portfolio.v2/utils"
 )
 
 func index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"services":   fetch.Current,
 		"monit_root": fetch.MONITROOT,
 	})
 }
 
-func ascii(c *gin.Context) {
-	c.HTML(http.StatusOK, "about.html", gin.H{
-		"services":   fetch.Current,
-		"monit_root": fetch.MONITROOT,
-	})
+func about(c *gin.Context) {
+	c.HTML(http.StatusOK, "about.tmpl", gin.H{})
 }
 
 func main() {
-	var err error
 
 	go fetch.Start()
 
-	tbox, _ := rice.FindBox("templates")
-
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
-	if err = utils.InitAssetsTemplates(r, tbox, nil, "index.html", "about.html"); err != nil {
-		log.Fatal(err)
-	}
+	r.Static("/static", "assets")
+	r.LoadHTMLGlob("templates/*")
+
 	r.GET("/", index)
-	r.GET("/about", ascii)
+	r.GET("/about", about)
+
 	r.Run(":8005")
 }
