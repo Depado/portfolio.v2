8905777151388cfa83929092f1eba0e29cb4d0fa
Depado <paul.lhussiez@gmail.com>
2016-04-12 15:28:54 +0200

    Conf and template

--- main.go
+++ main.go
@@ -1,35 +1,41 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 
+	"github.com/Depado/portfolio.v2/conf"
 	"github.com/Depado/portfolio.v2/fetch"
 )
 
-func index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-		"services":   fetch.Current,
-		"monit_root": fetch.MONITROOT,
-	})
-}
-
-func about(c *gin.Context) {
-	c.HTML(http.StatusOK, "about.tmpl", gin.H{})
-}
-
 func main() {
+	var err error
+
+	if err = conf.Load("conf.yml"); err != nil {
+		log.Fatal(err)
+	}
 
 	go fetch.Start()
 
-	gin.SetMode(gin.ReleaseMode)
+	if !conf.C.Debug {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	r := gin.Default()
 	r.Static("/static", "assets")
 	r.LoadHTMLGlob("templates/*")
 
-	r.GET("/", index)
-	r.GET("/about", about)
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"services":   fetch.Current,
+			"monit_root": fetch.MONITROOT,
+		})
+	})
+	r.GET("/about", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "about.tmpl", gin.H{})
+	})
 
-	r.Run(":8005")
+	r.Run(fmt.Sprintf(":%d", conf.C.Port))
 }
